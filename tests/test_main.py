a8f9128ad5573b9916c3771bc581ca4b2bdb0499
amaury5432 <amauryval@gmail.com>
2020-05-08 22:34:13 +0200

    add (tests) main test

---
+++ tests/test_main.py
@@ -0,0 +1,22 @@
+import geopandas as gpd
+
+from bokeh_for_map import BokehForMap
+
+
+def test_bokeh_processing(multipolygons_data):
+    width = 640
+    height = 800
+
+    my_map = BokehForMap("My beautiful map", width, height)
+
+    my_map.add_polygons(
+        multipolygons_data,
+        fill_color="orange",
+        legend="MultiPolygons"
+    )
+
+    assert my_map.figure.plot_height == height
+    assert my_map.figure.plot_width == width
+    assert len(my_map.figure.renderers) == 2
+    assert len(my_map.figure.tools) == 6
+    # TODO improve !
