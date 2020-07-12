b8f5e8bc5f861452fba1c51998d546d8c5c15c1a
amaury5432 <amauryval@gmail.com>
2020-07-12 11:41:02 +0200

    upd (meta.yaml) ok it works.... with blood...

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM continuumio/miniconda3
+
+
+#RUN conda update -n base -c defaults conda
+RUN conda install -y conda-build anaconda-client conda-verify
+
+# prepare app directory
+COPY . /home/app/
+WORKDIR /home/app/
--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 
-requirements = []
+#used by meta.yaml, do not forget space
+requirements = [
+    "geopandas",
+    "bokeh"
+]
 
 setup_requirements = []
 
@@ -9,24 +13,19 @@ test_requirements = []
 setup(
     author="amauryval",
     author_email='amauryval@gmail.com',
-    classifiers=[
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
-    ],
     description="An helper to map geodataframe on bokeh",
     entry_points={},
     install_requires=requirements,
     license="MIT license",
     long_description="",
     include_package_data=True,
-    keywords='bokeh',
+    keywords='geo bokeh',
     name='geo_bokeh',
-    packages=find_packages(include=['geo_bokeh']),
-    setup_requires=setup_requirements,
+    packages=find_packages(include=["geo_bokeh", "geo_bokeh.*"]),
+    # setup_requires=setup_requirements,
     test_suite='tests',
-    tests_require=test_requirements,
+    # tests_require=test_requirements,
     url='https://github.com/amauryval/geo_bokeh',
-    version='0.5.1',
+    version='0.5.3',
     zip_safe=False,
 )
