7b09e53fe33441ebec54f7a6a7c9b75aa1f2302b
amaury5432 <amauryval@gmail.com>
2020-07-16 21:01:20 +0200

    upd (*) input tests and color function based on layer name

--- easy_map_bokeh/main.py
+++ easy_map_bokeh/main.py
@@ -1,6 +1,5 @@
 import geopandas as gpd
-import random
-
+import hashlib
 
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
@@ -122,7 +121,7 @@ class EasyMapBokeh:
         bokeh_layer_container = self._format_gdf_features_to_bokeh(input_data)
 
         if color is None:
-            color = self._get_random_color()
+            color = self.__color_based_on_the_string_value(legend)
 
         rendered = self.figure.multi_line(
             xs="x",
@@ -163,7 +162,7 @@ class EasyMapBokeh:
         bokeh_layer_container = self._format_gdf_features_to_bokeh(input_data)
 
         if fill_color is None:
-            fill_color = self._get_random_color()
+            fill_color = self.__color_based_on_the_string_value(legend)
 
         rendered = getattr(self.figure, style)(
             x="x",
@@ -199,7 +198,7 @@ class EasyMapBokeh:
         bokeh_layer_container = self._format_gdf_features_to_bokeh(input_data)
 
         if fill_color is None:
-            fill_color = self._get_random_color()
+            fill_color = self.__color_based_on_the_string_value(legend)
 
         rendered = self.figure.multi_polygons(
             xs="x",
@@ -291,5 +290,15 @@ class EasyMapBokeh:
         bokeh_data = self.__convert_gdf_to_bokeh_data(input_gdf)
         return bokeh_data
 
-    def _get_random_color(self):
-        return random.choice(self.__BREWER_COLORS)
+
+    def __color_based_on_the_string_value(self , value):
+        """
+        To create a color based on the argument value (only string)
+
+        :param value: the string value
+        :type value: str
+
+        :return: hexadecimal color based on the value
+        :rtype: str
+        """
+        return f"#{hashlib.shake_256(bytes(value , encoding='utf-8')).hexdigest(3)}"
